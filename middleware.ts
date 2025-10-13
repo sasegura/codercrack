4b4b77a095f5ea7d2968cbc851fdb9d2fa49573a
Sergio Segura <sasegura.fernandez87@gmail.com>
2025-10-13 13:49:35 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,11 @@
 import createMiddleware from 'next-intl/middleware';
-import {locales, localePrefix} from './navigation';
+import {locales, localePrefix} from './src/navigation';
  
 export default createMiddleware({
   defaultLocale: 'es',
   locales,
-  localePrefix
+  localePrefix,
+  pathnames: {}
 });
  
 export const config = {
