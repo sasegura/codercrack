5b970c9a54d3d63aa4fea6bad96c67f373c3d82f
Sergio Segura <sasegura.fernandez87@gmail.com>
2025-10-16 11:38:29 +0000

    El dominio es .com

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://codercrack.dev',
+      url: 'https://codercrack.com',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
